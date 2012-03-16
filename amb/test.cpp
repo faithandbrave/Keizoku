57e3acfedf994c6968b83bf5a656d60456e4ab91
Akira Takahashi <faithandbrave@gmail.com>
2012-03-16 12:28:31 +0900

    add const.

--- amb/test.cpp
+++ amb/test.cpp
@@ -34,10 +34,10 @@ void amb3_f(amber& cont)
 
 int main()
 {
-    namespace fusion = boost::fusion;
+	namespace fusion = boost::fusion;
 
-    typedef fusion::vector<int, int, int> value_type;
-    std::vector<value_type> vec;
+	typedef fusion::vector<int, int, int> value_type;
+	std::vector<value_type> vec;
 
     AMB_BLOCK(ctx) {
         amber& x = amb(ctx, 0, amb1_f);
@@ -47,28 +47,29 @@ int main()
         vec.push_back(value_type(x.get(), y.get(), z.get()));
     };
 
-    std::vector<value_type> expected = boost::assign::list_of<value_type>
-        (1, 4, 6)
-        (1, 4, 7)
-        (1, 4, 8)
-        (1, 5, 6)
-        (1, 5, 7)
-        (1, 5, 8)
-        (2, 4, 6)
-        (2, 4, 7)
-        (2, 4, 8)
-        (2, 5, 6)
-        (2, 5, 7)
-        (2, 5, 8)
-        (3, 4, 6)
-        (3, 4, 7)
-        (3, 4, 8)
-        (3, 5, 6)
-        (3, 5, 7)
-        (3, 5, 8)
-        ;
+	const std::vector<value_type> expected = boost::assign::list_of<value_type>
+		(1, 4, 6)
+		(1, 4, 7)
+		(1, 4, 8)
+		(1, 5, 6)
+		(1, 5, 7)
+		(1, 5, 8)
+		(2, 4, 6)
+		(2, 4, 7)
+		(2, 4, 8)
+		(2, 5, 6)
+		(2, 5, 7)
+		(2, 5, 8)
+		(3, 4, 6)
+		(3, 4, 7)
+		(3, 4, 8)
+		(3, 5, 6)
+		(3, 5, 7)
+		(3, 5, 8)
+		;
 
-    BOOST_TEST(boost::equal(vec, expected));
+	BOOST_TEST(boost::equal(vec, expected));
 
-    return boost::report_errors();
+	return boost::report_errors();
 }
+
